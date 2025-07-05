1e2d7dd1e9d7ebc9c430cb33894d85189a799f04
Arcadius <mfn.45@outlook.com>
2025-07-05 14:05:10 +0300

    Inserted a task to track input from server and client terminals

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,80 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::Error, time::{self, Duration}};
+use futures_util::{future::join, SinkExt, StreamExt};
+use tokio::{io::{self, AsyncBufReadExt, BufReader}, sync::mpsc::{self, Receiver, Sender}, task::JoinHandle};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+
+
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() -> Result<(), Error> {
+
+    let (tx, rx) = mpsc::channel::<String>(1024);
+
+    let net_handle = net_helper(rx).await;
+
+    let inp_handle = inp_helper(tx).await;
+
+    let (_,_) = join(net_handle, inp_handle).await;
+
+    Ok(())
+}
+
+async fn net_helper(mut rx: Receiver<String>) -> JoinHandle<()> {
+    let url: &'static str = "ws://127.0.0.1:8883";
+
+    let t = time::UNIX_EPOCH;
+
+    let connection =  connect_async(url).await;
+    match connection {
+        Ok((wss,resp)) => {
+            let (mut write,mut read) = wss.split();
+            println!("Split WebSocket stream to sink and source!");
+
+            tokio::spawn({
+                async move {
+                    println!("Created network task!");
+                    // Receiving
+                    let recv_handle = tokio::spawn(async move {
+                            println!("Enabled Receiving!");
+                            while let Some(opt) = read.next().await {
+                                if let Ok(msg) = opt {
+                                    println!("Server: {}", msg.to_text().unwrap());
+                                } else if let Err(err) = opt {
+                                    println!("Found an error: {}", err);
+                                }
+                            }
+                        });
+                    
+                    // Sending
+                    let send_handle = tokio::spawn(async move {
+                            println!("Enabled Sending!");
+                            if let Some(msg) = rx.recv().await {
+                                write.send(Message::Text(msg.into())).await.unwrap();
+                            }
+                        });
+
+                    let (_,_) = join(recv_handle, send_handle).await;
+
+                }
+            })
+        },
+        Err(err) => { println!("[Error] Failed to connected to {}!", url); Ok(()) }
+    }
+}
+
+async fn inp_helper(tx: Sender<String>) -> JoinHandle<()> {
+    tokio::spawn({
+        async move {
+            println!("Created input task!");
+            let reader = BufReader::new(io::stdin());
+            let mut lines = reader.lines();
+            
+            while let Ok(Some(msg)) = lines.next_line().await {
+                let send = tx.send(msg).await;
+                if let Err(a) = send {
+                    println!("Error sending message: {}", a.to_string());
+                }
+            }
+        }
+    })
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,30 +1,62 @@
-use std::time::Duration;
-
-use futures_util::{SinkExt, StreamExt};
-use tokio::net::TcpListener;
+use futures_util::{future::join, SinkExt, StreamExt};
+use tokio::{io::{self, AsyncBufReadExt, BufReader}, net::TcpListener};
 use tokio_tungstenite::tungstenite::Message;
+use tokio::sync::broadcast::channel;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
-    let tcp = TcpListener::bind("127.0.0.1:8883").await.unwrap();
-    println!("Successfully binded to 127.0.0.1:8883! Waiting for connections!");
     
-    while let Ok((stream, addr)) = tcp.accept().await {
-        println!("Client at {}:{} has connected!", addr.ip().to_string(), addr.port());
-        tokio::spawn(async move {
-            let s = tokio_tungstenite::accept_async(stream).await.unwrap();
-            let (mut write,mut read) = s.split();
+    let tcp = TcpListener::bind("127.0.0.1:8883").await.unwrap();
 
-            tokio::spawn(async move {
-                loop {
-                    write.send(Message::Text("Hi there I am server!".into())).await.unwrap();
-                    tokio::time::sleep(Duration::from_secs(2)).await;
-                }
-            });
+    println!("Successfully binded to 127.0.0.1:8883! Waiting for connections!");
 
-            while let Ok(msg) = read.next().await.unwrap() {
-                println!("{}:{} -> {}", addr.ip().to_string(), addr.port(), msg.to_text().unwrap());
+    let (tx,mut _rx) = channel::<String>(16);
+
+    let inp_handle = tokio::spawn({
+        let tx_net = tx.clone();
+        async move {
+            let stdin = BufReader::new(io::stdin());
+            let mut lines = stdin.lines();
+            while let Ok(Some(msg)) = lines.next_line().await {
+                tx_net.send(msg.to_string()).unwrap();
             }
-        });
+        }
+    });
+    
+    let net_handle = async move {
+        while let Ok((stream, addr)) = tcp.accept().await {
+            println!("Client at {}:{} has connected!", addr.ip().to_string(), addr.port());
+            tokio::spawn({
+                let tx_net = tx.clone();
+                async move {
+                    let s = tokio_tungstenite::accept_async(stream).await.unwrap();
+                    let (mut write,mut read) = s.split();
+                    write.send(Message::Text("Welcome to the server!".into())).await.unwrap();
+
+                    let mut rx_peer = tx_net.subscribe();
+
+                    // Receiving
+                    let recv_handle = tokio::spawn(async move {
+                        println!("Receiving task enabled!");
+                        while let Ok(msg) = read.next().await.unwrap() {
+                            tx_net.send(msg.to_string()).unwrap();
+                            println!("{}:{} -> {}", addr.ip().to_string(), addr.port(), msg.to_text().unwrap());
+                        }
+                    });
+
+                    // Sending
+                    let send_handle = tokio::spawn(async move {
+                        println!("Sending task enabled!");
+                        while let Ok(msg) = rx_peer.recv().await {
+                            write.send(Message::Text(msg.into())).await.unwrap();
+                        }
+                    });
+
+                    let (_,_) = join(recv_handle, send_handle).await;
+                }
+            }).await.unwrap();
     }
+    };
+
+    let (_,_) = join(net_handle,inp_handle).await;
 }
